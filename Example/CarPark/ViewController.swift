f34cdd0a468b73083328ecb4294dc08e6c074126
HerlanggaWibi <52308348+HerlanggaWibi@users.noreply.github.com>
2019-07-04 15:40:01 +0700

    Update ViewController.swift

--- Example/CarPark/ViewController.swift
+++ Example/CarPark/ViewController.swift
@@ -7,18 +7,16 @@
 //
 
 import UIKit
+import CarPark
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationController?.pushViewController(CarParkLibrary.listDefault(), animated: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 }
 
