02ba0ddc02af76c8f805da4f088aba8a3cdb6df8
HerlanggaWibi <52308348+HerlanggaWibi@users.noreply.github.com>
2019-07-05 21:46:29 +0700

    Add files via upload

---
+++ Classes/CarParkModel.swift
@@ -0,0 +1,15 @@
+//
+//  CarParkModel.swift
+//  CarPark
+//
+//  Created by herlangga wibi on 05/07/19.
+//  Copyright © 2019 herlangga wibi. All rights reserved.
+//
+
+import Foundation
+
+struct CarParkModel {
+    var lot_type: String
+    var lots_available: String
+    var total_lots: String
+}
